147cbe07730e576f0bd800c0d395b1d11a6baa78
Ian Ramos <ianramossantos@hotmail.com>
2022-01-21 18:12:02 -0300

    feat: list all repositories from a github profile

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,17 +4,11 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
+    fetch('https://api.github.com/users/i-ramoss/repos')
     .then(response => response.json())
     .then(data => setRepositories(data))
   }, []);
@@ -24,10 +18,9 @@ export function RepositoryList() {
       <h1>Repository List</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+        })}
       </ul>
     </section>
   );
